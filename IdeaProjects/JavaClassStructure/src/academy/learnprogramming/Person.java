347e0851419de38aa9e0ace42627048af97be473
Víctor Câmara <victor.camara@ifactory.com.br>
2018-07-16 10:43:05 -0300

    First class of the certification course

---
+++ IdeaProjects/JavaClassStructure/src/academy/learnprogramming/Person.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public void getFirstName(String firstName) {
+        this.firstName = firstName
+    }
+
+}
