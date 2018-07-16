d9928b409b085797b7d2aea48acc13fe190e0edb
Víctor Câmara <victor.camara@ifactory.com.br>
2018-07-16 13:57:01 -0300

    Second project in the certification course, This is from the second class and the third one will be in this project too

---
+++ IdeaProjects/MainMethod/src/academy/learnprogramming/Main.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("args-size " + args.length);
+
+        for (int i = 0; i < args.length; i++){
+            System.out.println("args[" + i + "]= " + args[i]);
+        }
+    }
+}
